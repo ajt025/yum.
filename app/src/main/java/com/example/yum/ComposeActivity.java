7b01cf2024ef4d5d4630da400ca51b19294e12f5
Chris Jiang <ChrisJiang@chriss-mbp-8.lan>
2019-11-16 21:02:54 -0800

    Fleshed out review functionality with photo insertion

--- app/src/main/java/com/example/yum/ComposeActivity.java
+++ app/src/main/java/com/example/yum/ComposeActivity.java
@@ -1,27 +1,46 @@
 package com.example.yum;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.yum.fragments.ExploreFragment;
 import com.example.yum.models.Food_Review_Database;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.FileNotFoundException;
 
 
 public class ComposeActivity extends AppCompatActivity {
 
+    private Button btnInsertImage;
     private Button btnSubmit;
     private EditText cmpTitle;
     private EditText cmpRating;
     private EditText cmpDescription;
+    private ImageView viewImage;
     private DatabaseReference myDatabase;
+    private Uri targetUri;
+    private Bitmap bitmap;
     Food_Review_Database foodObject;
 
     @Override
@@ -29,12 +48,28 @@ public class ComposeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
 
+        btnInsertImage = findViewById(R.id.btnImageInsert);
         btnSubmit = findViewById(R.id.btnSubmit);
         cmpTitle = findViewById(R.id.cmpTitle);
         cmpRating = findViewById(R.id.cmpRating);
         cmpDescription = findViewById(R.id.cmpDescription);
+        viewImage = findViewById(R.id.imageView);
 
         myDatabase = FirebaseDatabase.getInstance().getReference().child("Reviews");
+
+
+        // pick image of food
+        btnInsertImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images
+                .Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, 0);
+            }
+
+        });
+
 
         // Submitting data to firebase
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +85,6 @@ public class ComposeActivity extends AppCompatActivity {
                 //constructing data object here
                 foodObject = new Food_Review_Database();
 
-                foodObject.setReview_title(title);
                 foodObject.setReview_body(description);
                 foodObject.setRating(rating);
                 foodObject.setReview_id(id);
@@ -58,7 +92,8 @@ public class ComposeActivity extends AppCompatActivity {
                 foodObject.setDownvote_count(0);
 
                 // send it to firebase
-                myDatabase.push().setValue(foodObject);
+                myDatabase.child(title).setValue(foodObject);
+                uploadPicture();
 
                 Toast.makeText(ComposeActivity.this, "Review Submitted", Toast.LENGTH_LONG).show();
 
@@ -70,7 +105,73 @@ public class ComposeActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    // helper function to display food image
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            targetUri = data.getData();
+
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                viewImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // essentially uploads the picture to firebase storage
+    private void uploadPicture() {
+
+        // progress bar of uploading picture
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading");
+        progressDialog.show();
+
+        // creating unique path for review
+        String path = "reviewImages/";
+        String id = myDatabase.push().getKey();
+        path += id;
+        path += ".jgp";
+
+        StorageReference riversRef = FirebaseStorage.getInstance().getReference()
+                .child(path);
+        riversRef.putFile(targetUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //if the upload is successful
+                        //hiding the progress dialog
+                        progressDialog.dismiss();
+
+                        //and displaying a success toast
+                        Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        //if the upload is not successful
+                        //hiding the progress dialog
+                        progressDialog.dismiss();
+
+                        //and displaying error message
+                        Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        //calculating progress percentage
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+
+                        //displaying percentage in progress dialog
+                        progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+                    }
+                });
 
     }
 }
--- app/src/main/java/com/example/yum/models/Food_Review_Database.java
+++ app/src/main/java/com/example/yum/models/Food_Review_Database.java
@@ -2,13 +2,9 @@ package com.example.yum.models;
 
 public class Food_Review_Database {
     public String review_id;
-    //public int user_id;
-    //public String img_path;
-    public String review_title;
     public String review_body;
     public int upvote_count;
     public int downvote_count;
-    //public int food_and_restaurant_id;
     public double rating;
 
     public Food_Review_Database() {}
@@ -19,14 +15,6 @@ public class Food_Review_Database {
 
     public void setReview_id(String review_id) {
         this.review_id = review_id;
-    }
-
-    public String getReview_title() {
-        return review_title;
-    }
-
-    public void setReview_title(String review_title) {
-        this.review_title = review_title;
     }
 
     public String getReview_body() {
