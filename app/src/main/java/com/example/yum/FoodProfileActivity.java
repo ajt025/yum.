c0ec55aeb1a1b16a591e5b4dc064bc30ac897e4f
Anthony Joseph Tran <ajt025@ucsd.edu>
2019-11-18 01:19:26 -0800

    last minute push for sample screens

--- app/src/main/java/com/example/yum/FoodProfileActivity.java
+++ app/src/main/java/com/example/yum/FoodProfileActivity.java
@@ -1,20 +1,22 @@
 package com.example.yum;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.yum.models.Restaurant;
 import com.example.yum.models.Review;
 
 import java.util.ArrayList;
 
 public class FoodProfileActivity extends AppCompatActivity {
 
-    RecyclerView rvReviews;
-    ReviewAdapter reviewAdapter;
-    ArrayList<Review> reviews;
+    RecyclerView rvRestaurants;
+    RestaurantAdapter restaurantAdapter;
+    ArrayList<Restaurant> restaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +24,28 @@ public class FoodProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_food_profile);
 
         // View setup + RV initialization
-        rvReviews = findViewById(R.id.rvReviews);
-        reviews = new ArrayList<>();
-        reviewAdapter = new ReviewAdapter(reviews);
+        rvRestaurants = findViewById(R.id.rvRestaurants);
+        restaurants = new ArrayList<>();
+        restaurantAdapter = new RestaurantAdapter(restaurants);
 
-        rvReviews.setLayoutManager(new LinearLayoutManager(this));
-        rvReviews.setAdapter(reviewAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
-        populateReviews(); // call to put reviews into RV
+        rvRestaurants.setLayoutManager(layoutManager);
+        rvRestaurants.setAdapter(restaurantAdapter);
+        rvRestaurants.addItemDecoration(new DividerItemDecoration(this, layoutManager.getOrientation()));
+
+        populateRestaurants(); // call to put reviews into RV
     }
 
     // HELPER METHODS
 
-    private void populateReviews() {
+    private void populateRestaurants() {
         /*
          * TODO remove this, just test code. Here is where you would make database calls and retrieve reviews
          */
         for (int i = 0; i < 10; ++i) {
-            reviews.add(new Review());
-            reviewAdapter.notifyItemInserted(reviews.size() - 1); // tells rv to check for updates
+            restaurants.add(new Restaurant());
+            restaurantAdapter.notifyItemInserted(restaurants.size() - 1); // tells rv to check for updates
         }
     }
 }
---
+++ app/src/main/java/com/example/yum/RestaurantAdapter.java
@@ -0,0 +1,69 @@
+package com.example.yum;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.yum.models.Restaurant;
+import com.example.yum.models.Review;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder> {
+
+    private List<Restaurant> mRestaurants;
+    Context context;
+
+    // pass in reviews into the constructor for RV
+    public RestaurantAdapter(List<Restaurant> restaurants) {
+        mRestaurants = restaurants;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View reviewView = inflater.inflate(R.layout.item_restaurant, parent, false);
+        ViewHolder viewHolder = new ViewHolder(reviewView);
+        return viewHolder;
+    }
+
+    // bind values based on the position of each element
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Restaurant restaurant = mRestaurants.get(position);
+
+        // TODO this is where you will load images/text/etc into the review RecyclerView
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRestaurants.size();
+    }
+
+    // ViewHolder class, handles connection of views to vars and listener setup
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final Intent intent = new Intent(context, RestaurantActivity.class);
+                    intent.putExtra("restaurant", Parcels.wrap(mRestaurants.get(getAdapterPosition())));
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+    }
+}
