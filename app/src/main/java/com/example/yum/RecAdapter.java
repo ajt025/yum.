c6dda709557022e321c9be596e68cfe3282c73d9
Anthony Joseph Tran <ajt025@ucsd.edu>
2019-12-06 15:59:16 -0800

    cleaning imports

--- app/src/main/java/com/example/yum/RecAdapter.java
+++ app/src/main/java/com/example/yum/RecAdapter.java
@@ -15,7 +15,6 @@ import com.example.yum.models.Review;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class RecAdapter extends RecyclerView.Adapter<RecAdapter.ViewHolder> {
 
